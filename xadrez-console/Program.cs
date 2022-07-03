0bb9f3d18d085c4db387ece1789752f0dad3946d
leandro silva <leandro3903@gmail.com>
2022-07-03 20:52:37 -0300

    Tratando erros com exceção personalizada

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -15,7 +15,7 @@ namespace xadrez_console
             Tabuleiro tab = new Tabuleiro(8, 8);
             tab.colocarPeca(new Torre(Cor.Preta, tab), new Posicao(0, 0));
             tab.colocarPeca(new Torre(Cor.Preta,tab), new Posicao(1, 3));
-            tab.colocarPeca(new Rei(Cor.Preta, tab), new Posicao(2, 4));
+            tab.colocarPeca(new Rei(Cor.Preta, tab), new Posicao(0,0));
 
             Tela.imprimirTabuleiro(tab);
 
